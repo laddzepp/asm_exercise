68a805cf92459e193f612d89318448c6a0fc83a0
Vladislav Grigoryev <grigorevvlad207@gmail.com>
2025-05-26 04:28:47 +0300

    practice4

---
+++ practice4/7_1.asm
@@ -0,0 +1,48 @@
+section .data
+	format db "%d",0xA
+	;array dd 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3
+	array dd 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+	;array dd 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+	res dd 50 dup(0)
+
+section .text
+	global main
+	extern printf
+
+main:
+	mov rcx, 0
+	mov rdx, 0
+for:
+	mov rax, [array + rcx * 4]
+	mov rbx, rax
+	and rax, 0x1
+	cmp rax, 0
+	jz is_even
+is:
+	mov [res + rdx * 4], rbx
+	inc rdx
+is_even:
+	inc rcx
+	cmp rcx, 0xF
+	jl for
+	cmp rdx, 0 
+	jz exit
+
+	push rdx
+	xor rcx, rcx
+	push rcx
+print_array:
+	mov rdi, format
+	mov rsi, [res + rcx * 4]
+	xor rax, rax
+	call printf
+	pop rcx
+	inc rcx
+	push rcx
+	cmp rcx, [rsp+8]
+	jl print_array
+	pop rcx
+	pop rcx
+exit:
+	mov rax, 0
+	ret
---
+++ practice4/7_2.asm
@@ -0,0 +1,87 @@
+segment .data
+
+	a dq 2
+	cnt dq 0
+	fmt dq "%d ",10, 0
+	fmt_in dq "%d", 0
+
+segment .bss
+
+	array resq 21
+
+segment .text
+
+	global main
+	extern printf
+	extern scanf
+
+main:
+
+push rbp
+
+xor rax, rax
+xor rcx, rcx
+xor rbx, rbx
+
+input_array:
+	cmp rcx, 0xF
+	jz done
+	mov [cnt], rcx
+	mov rax, 0
+	mov rdi, fmt_in
+	mov rsi, a
+	call scanf
+	mov rax, [a]
+	mov rcx, [cnt]
+	mov [array+rcx*8], rax
+	add rbx, [a]
+	inc rcx
+	jmp input_array
+
+done:
+        xor rax, rax
+	xor rcx, rcx
+	xor rbx, rbx
+
+outer_loop:
+	cmp rcx, 0xF
+	jge end_loop
+	mov [cnt], rcx
+	mov rax, [array+rcx*8]
+
+inner_loop:
+	inc rcx
+	cmp rcx, 0xF
+	jz ok
+	cmp rax, [array+rcx*8]
+	jle inner_loop
+	xchg rax, [array+rcx*8]
+	jmp inner_loop
+
+ok:
+	mov rcx, [cnt]
+	mov [array+rcx*8], rax
+	inc rcx
+	jmp outer_loop
+
+end_loop:
+        xor rax, rax
+        xor rbx, rbx
+        xor rcx, rcx
+
+	mov rdi, fmt
+	mov rsi, [array+14*8]
+	call printf
+
+	xor rax, rax
+	xor rbx, rbx
+	xor rcx, rcx
+
+	mov rdi, fmt
+	mov rsi, [array + 13*8]
+	call printf
+
+end:
+	mov rax, 0
+	pop rbp
+ret
